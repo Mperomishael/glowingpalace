65ce53ce6b37ed539f3636487e101ea44431cbf7
Mishael Yakubu <147824425+Mperomishael@users.noreply.github.com>
2026-09-01 19:46:57 -1100

    Change Link import from @tanstack/react-router to react-router-dom

--- gpcm-app/src/components/GallerySection.tsx
+++ gpcm-app/src/components/GallerySection.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "@tanstack/react-router";
+import { Link } from "react-router-dom";
 import { X, ArrowRight } from "lucide-react";
 import { useMedia } from "../hooks/useMedia";
 import { useScrollReveal, staggerDelay } from "../hooks/useScrollReveal";
